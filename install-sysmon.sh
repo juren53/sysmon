3000853979904ff06edc7f87afc77e92e31782f4
Jim U'Ren <jim.uren@gmail.com>
2025-12-04 14:02:30 -0600

    Delete install-sysmon.sh
    Remove legacy files uploaded via web interface

--- install-sysmon.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-curl -O https://raw.githubusercontent.com/juren53/sysmon/main/sysmon.py
-chmod 755 sysmon.py
-cp sysmon.py ~/.local/bin/sysmon
-echo "sysmon now installed in .local/bin"
